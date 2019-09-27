a2976e5f6b4215a7524ec01bd7c3dabd2312c67c
Lars Kiesow <lkiesow@uos.de>
2019-09-27 16:53:31 +0200

    Read data files
    This fixes #2

--- emissionsapi/preprocess.py
+++ emissionsapi/preprocess.py
@@ -1,7 +1,21 @@
 import os
-from os.path import isfile, join
+import gdal
+import numpy
 
 PATH = 'data'
+
+# Specify the layer name to read
+LAYER_NAME = '//PRODUCT/carbonmonoxide_total_column'
+LONGITUDE_NAME = '//PRODUCT/longitude'
+LATITUDE_NAME = '//PRODUCT/latitude'
+QA_VALUE_NAME = '//PRODUCT/qa_value'
+
+class Scan():
+    data = []
+    longitude = []
+    latitude = []
+    quality = []
+
 
 def list_ncfiles():
     for f in os.listdir(PATH):
@@ -9,8 +23,23 @@ def list_ncfiles():
         if os.path.isfile(filepath) and filepath.endswith('.nc'):
             yield filepath
 
+
 def read_file(ncfile):
-    return {}
+    scan = Scan()
+    ds = gdal.Open(f'HDF5:{ncfile}:{LAYER_NAME}')
+    scan.data = numpy.ndarray.flatten(ds.ReadAsArray())
+
+    ds = gdal.Open(f'HDF5:{ncfile}:{LONGITUDE_NAME}')
+    scan.longitude = numpy.ndarray.flatten(ds.ReadAsArray())
+
+    ds = gdal.Open(f'HDF5:{ncfile}:{LATITUDE_NAME}')
+    scan.latitude = numpy.ndarray.flatten(ds.ReadAsArray())
+
+    ds = gdal.Open(f'HDF5:{ncfile}:{QA_VALUE_NAME}')
+    scan.quality = numpy.ndarray.flatten(ds.ReadAsArray())
+
+    return scan
+
 
 def filter_data(data):
     return data
@@ -22,6 +51,7 @@ def entrypoint():
     for ncfile in list_ncfiles():
         print(ncfile)
         data = read_file(ncfile)
+        print(data)
         data = filter_data(data)
         write_to_database(data)
     pass
