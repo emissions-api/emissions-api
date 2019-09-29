1283fc7b2f74fe76fe7f869bcdc68970571ef71c
Lars Kiesow <lkiesow@uos.de>
2019-09-29 23:07:12 +0200

    Load Configuration File
    This patch adds configuration file handling. Using this, we can easily
    load configuration values like this:
    
    ```python
    from emissionsapi.config import config
    path = config('data-path') or 'data'
    ```
    
    The configuration file locations taken into consideration are (in this
    particular order):
    
    - `./emissionsapi.yml`
    - `~/emissionsapi.yml`
    - `/etc/emissionsapi.yml`
    
    Still open is the question how this relates to configuration stored in
    environment variables as they are used right now in the database
    modules:
    
    ```python
    database = os.environ.get(
         'EMISSIONS_API',
         'postgresql://user:user@localhost/db')
    ```
    
    Does it make sense to use the environment variables if no configuration
    file exists? Or should an environment variable overwrite the
    configuration file?
    
    This fixes #11

---
+++ emissionsapi/config.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+'''
+Load and handle Emission API configuration.
+'''
+
+import yaml
+import os
+
+__config = {}
+
+
+def configuration_file():
+    '''Find the best match for the configuration file.  The configuration file
+    locations taken into consideration are (in this particular order):
+
+    - ./emissionsapi.yml
+    - ~/emissionsapi.yml
+    - /etc/emissionsapi.yml
+
+    :return: configuration file name or None
+    '''
+    if os.path.isfile('./emissionsapi.yml'):
+        return './emissionsapi.yml'
+    if os.path.isfile('~/emissionsapi.yml'):
+        return '~/emissionsapi.yml'
+    if os.path.isfile('/etc/emissionsapi.yml' ):
+        return '/etc/emissionsapi.yml'
+
+
+def update_configuration():
+    '''Update configuration.
+    '''
+    cfgfile = configuration_file()
+    if not cfgfile:
+        return {}
+    with open(cfgfile, 'r') as f:
+        cfg = yaml.safe_load(f)
+    globals()['__config'] = cfg
+    return cfg
+
+
+def config(key=None):
+    '''Get a specific configuration value or the whole configuration, loading
+    the configuration file if it was not before.
+
+    :param key: optional configuration key to return
+    :type key: string
+    :return: dictionary containing the configuration or configuration value
+    '''
+    cfg = __config or update_configuration()
+    return cfg.get(key) if key else cfg
