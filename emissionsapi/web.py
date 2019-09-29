d7f7f98443f52ebc507a72b623fcb11bdd1942b9
Sven Haardiek <sven@haardiek.de>
2019-09-29 17:58:29 +0200

    Implement REST API call with rectangle #7
    Signed-off-by: Sven Haardiek <sven@haardiek.de>

--- emissionsapi/web.py
+++ emissionsapi/web.py
@@ -1,8 +1,11 @@
 """Web Application to deliver the data stored in the database via an API to
 the users.
 """
-from flask import Flask, jsonify
+from flask import Flask, request, Response
+import geojson
 
+
+import emissionsapi.db
 import emissionsapi.logger
 
 # Logger
@@ -13,8 +16,44 @@ app = Flask(__name__)
 
 
 @app.route('/api/v1/geo.json')
-def get_data():
-    return jsonify({})
+@emissionsapi.db.with_session
+def get_data(session):
+    """Get data in geojson format.
+
+    Mandatory url parameter 'geoparam' with 4 comma seperated values.
+    Those 4 value are the longitude and latidute of the upper left and
+    lower right points a the rectangle. The Response contains all points
+    from the database from withing this rectangle.
+
+    You can try this with
+    curl http://127.0.0.1:5000/api/v1/geo.json?geoparam=15,45,20,40
+
+    :param session: SQL Alchemy Session
+    :type session: sqlalchemy.orm.session.Session
+    :return: Geojson Response as feature collection
+    :rtype: flask.Response
+    """
+    # Get and parse url parameter
+    try:
+        lo1, lat1, lo2, lat2 = [float(x) for x in request.args.get(
+            "geoparam", "").split(",")]
+    except ValueError:
+        return '', 400
+
+    # Init feature list
+    features = []
+    # Iterate through database query
+    for feature in emissionsapi.db.get_points_in_rectangle(
+            session, (lo1, lat1), (lo2, lat2)):
+        # Create and append single features.
+        features.append(geojson.Feature(
+            geometry=geojson.Point((feature.longitude, feature.latitude)),
+            properties={"carbonmonixide": feature.value}))
+    # Create feature collection from gathered points
+    feature_collection = geojson.FeatureCollection(features)
+    return Response(
+        geojson.dumps(feature_collection, sort_keys=True),
+        mimetype='application/json')
 
 
 def entrypoint():
