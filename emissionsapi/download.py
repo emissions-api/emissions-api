383757898156cfc10521f7cc8892e1e8318072d0
Lars Kiesow <lkiesow@uos.de>
2019-09-29 22:34:29 +0200

    Download sample data
    This patch ensures the data directory exists and downloads a single
    sample data set.
    
    Related to #1

--- emissionsapi/download.py
+++ emissionsapi/download.py
@@ -2,6 +2,7 @@
 """
 
 import os
+import urllib3
 
 import emissionsapi.logger
 
@@ -10,7 +11,17 @@ logger = emissionsapi.logger.getLogger('emission-api.download')
 
 
 def download():
-    os.makedirs('data')
+    os.makedirs('data', exist_ok=True)
+
+    # TODO: Replace this with sentinalsat
+    filename = 'S5P_NRTI_L2__CO_____20190921T122303_20190921T122803_'\
+            + '10046_01_010302_20190921T130405.nc'
+    url = 'https://data.lkiesow.io/emissions-api/' + filename
+    connection_pool = urllib3.PoolManager()
+    resp = connection_pool.request('GET', url)
+    with open('data/' + filename, 'wb') as f:
+        f.write(resp.data)
+    resp.release_conn()
 
 
 if __name__ == "__main__":
