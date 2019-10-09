e1907e7baf68aeca609df3ceb06d6e1e4b75d326
Lars Kiesow <lkiesow@uos.de>
2019-10-09 22:54:52 +0200

    Remove configuration for listening address
    This patch removes the configuration for the listening address, making
    the API's development server listen to localhost only.

--- emissionsapi/web.py
+++ emissionsapi/web.py
@@ -6,7 +6,6 @@ import geojson
 
 import emissionsapi.db
 import emissionsapi.logger
-from emissionsapi.config import config
 from emissionsapi.country_bounding_boxes import country_bounding_boxes
 
 # Logger
@@ -68,7 +67,7 @@ def entrypoint():
     production, but only during the development.
     """
     logger.info("Starting the Connexion Debug Server")
-    app.run(host=config('host') or '127.0.0.1')
+    app.run(host='127.0.0.1')
 
 
 if __name__ == "__main__":
