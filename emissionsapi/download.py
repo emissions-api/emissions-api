30a7b62d24fea1bb677255e104da523b1d8d9994
Sven Haardiek <sven@haardiek.de>
2019-10-24 22:42:28 +0200

    Download not limited by Countries
    Currently it is only possible to download data limited by countries. This patch
    introduces a special country code 'all' which disables this limit and download
    all data.
    
    Signed-off-by: Sven Haardiek <sven.haardiek@iotec-gmbh.de>

--- emissionsapi/download.py
+++ emissionsapi/download.py
@@ -23,6 +23,36 @@ download_logger.setLevel(logger.getEffectiveLevel())
 
 product = 'L2__CO____'
 processing_level = 'L2'
+storage = config('storage') or 'data'
+
+
+def filtered_download(polygon=None, begin_ts=None, end_ts=None):
+    """Download Sentinel 5 data from the ESA and store them in the data
+    directory.
+    The downloaded files are optionally filtered by location, start and end
+    time.
+
+    :param polygon: WKT polygon specifying an area the data should intersect,
+        defaults to None
+    :type polygon: str, optional
+    :param begin_ts: ISO-8601 timestamp specifying the earliest sensing date,
+        defaults to None
+    :type begin_ts: str, optional
+    :param end_ts: ISO-8601 timestamp specifying the latest sensing date,
+        defaults to None
+    :type end_ts: str, optional
+    """
+    # Search for data matching the parameter
+    result = sentinel5dl.search(
+            polygon=polygon,
+            begin_ts=begin_ts,
+            end_ts=end_ts,
+            product=product,
+            processing_level=processing_level)
+    logger.info('Found {0} products'.format(len(result.get('products'))))
+
+    # Download data
+    sentinel5dl.download(result.get('products'), output_dir=storage)
 
 
 def download():
@@ -35,26 +65,30 @@ def download():
     date_begin = config('download', 'date', 'begin')\
         or '2019-09-10T00:00:00.000Z'
     date_end = config('download', 'date', 'end') or '2019-09-11T00:00:00.000Z'
-    countries = config('download', 'country') or ['DE']
+    countries = config('download', 'country') or None
 
     # create storage folder if not existing
     os.makedirs(storage, exist_ok=True)
 
+    if countries is None:
+        logger.info(
+            f'Looking for products between {date_begin} and {date_end}.')
+        filtered_download(
+            begin_ts=date_begin,
+            end_ts=date_end,
+        )
+        return
+
     for country in countries:
-        wkt = bounding_box_to_wkt(*country_bounding_boxes[country][1])
-
-        # Search for data matching the parameter
-        logger.info(f'Looking for products in country {country}')
-        result = sentinel5dl.search(
-                wkt,
-                begin_ts=date_begin,
-                end_ts=date_end,
-                product=product,
-                processing_level=processing_level)
-        logger.info('Found {0} products'.format(len(result.get('products'))))
-
-        # Download data
-        sentinel5dl.download(result.get('products'), output_dir=storage)
+        polygon = bounding_box_to_wkt(*country_bounding_boxes[country][1])
+        logger.info(
+            f'Looking for products in country {country} '
+            f'between {date_begin} and {date_end}.')
+        filtered_download(
+            polygon=polygon,
+            begin_ts=date_begin,
+            end_ts=date_end,
+        )
 
 
 if __name__ == "__main__":
