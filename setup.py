8954fdc590b8269c6238c8fdb6cf6f67f7637075
Sven Haardiek <sven@haardiek.de>
2019-11-03 14:46:33 +0100

    Adding more information to the setup.py
    This patch does add more information to the setup.py and make it possible to
    install this package inclusive the dependencies without calling `pip install -r
    requirements.txt` first.

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 from setuptools import setup, find_packages
+import os
+
+path = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(filename):
+    with open(os.path.join(path, filename), encoding='utf-8') as f:
+        return f.read()
+
 
 setup(
     name='emissions-api',
+    author='Emissions API Developers',
+    license='MIT',
+    url='https://github.com/emissions-api/emissions-api',
     packages=find_packages(),
+    install_requires=read('requirements.txt').split(),
+    long_description=read('README.rst'),
+    long_description_content_type='text/x-rst',
     entry_points={
         'console_scripts': [
             'emissionsapi-download=emissionsapi.download:download',
