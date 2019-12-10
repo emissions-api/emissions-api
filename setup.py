2b68efa51be36659056282d39f8407c7a0fb7af9
Sven Haardiek <sven@haardiek.de>
2019-12-10 23:18:13 +0100

    Requirements Rework
    This patch splits the required dependencies in the `setup.py` and in the `requirements.txt`.
    
    `setup.py` now contains a minimal set, which is needed to install emissions-api as a module.
    
    `requirements.txt` contains a full list of dependencies including the
    dependencies of dependencies and packages only needed for the installation like
    `gunicorn`.

--- setup.py
+++ setup.py
@@ -8,14 +8,22 @@ def read(filename):
     with open(os.path.join(path, filename), encoding='utf-8') as f:
         return f.read()
 
-
 setup(
     name='emissions-api',
     author='Emissions API Developers',
     license='MIT',
     url='https://github.com/emissions-api/emissions-api',
     packages=find_packages(),
-    install_requires=read('requirements.txt').split(),
+    install_requires=[
+        'psycopg2',
+        'connexion',
+        'GeoAlchemy2',
+        'geojson',
+        'python-dateutil',
+        'sentinel5dl',
+        's5a',
+        'SQLAlchemy',
+    ],
     long_description=read('README.rst'),
     long_description_content_type='text/x-rst',
     entry_points={
