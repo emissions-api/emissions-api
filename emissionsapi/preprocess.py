511a6b3957d36d8700e90d27de989850069ade2a
Sven Haardiek <sven@haardiek.de>
2020-01-27 21:48:45 +0100

    Parallel processing
    This patchs enabled parallel processing of the files during the preprocessing.
    
    It introduces a new config parameter to control how many worker are used for processing.

--- emissionsapi/preprocess.py
+++ emissionsapi/preprocess.py
@@ -5,6 +5,7 @@
 """Preprocess the locally stored data and store them in the database.
 """
 import logging
+import multiprocessing
 import os
 
 import s5a
@@ -17,6 +18,9 @@ logger = logging.getLogger(__name__)
 
 # Path where to store the data
 storage = config('storage') or 'data'
+
+# Number of workers to preprocess
+workers = config('workers') or 1
 
 
 @emissionsapi.db.with_session
@@ -63,22 +67,39 @@ def write_to_database(session, data, filepath):
     session.commit()
 
 
+def preprocess_file(ncfile):
+    '''Preprocess a single file and write it to the database
+
+    :param ncfile: path to the ncfile to preprocess
+    :type ncfile: str
+    '''
+    logger.info("Reading file '%s'", ncfile)
+    scan = s5a.load_ncfile(ncfile)
+
+    logger.info("Filtering %s points by quality of file '%s'", len(scan),
+                ncfile)
+    scan = s5a.filter_by_quality(scan)
+
+    # Skip file if no points are present after filtering
+    if len(scan) == 0:
+        logger.warning("No points left after filtering of '%s'", ncfile)
+        return
+
+    logger.info("Writing %s points from '%s' to database", len(scan), ncfile)
+    write_to_database(scan, ncfile)
+
+    logger.info("Finished writing points from '%s' to database", ncfile)
+
+
 def main():
     """Entrypoint for running this as a module or from the binary.
     Triggers the preprocessing of the data.
     """
     # Iterate through all find nc files
-    for ncfile in sorted(list_ncfiles()):
-        logger.info("Reading file '%s'", ncfile)
-        scan = s5a.load_ncfile(ncfile)
+    with multiprocessing.Pool(workers) as p:
+        p.map(preprocess_file, sorted(list_ncfiles()))
 
-        logger.info("Filtering %s points by quality", scan.size)
-        scan = s5a.filter_by_quality(scan)
-
-        logger.info('Writing %s points to database', scan.size)
-        write_to_database(scan, ncfile)
-
-        logger.info('Finished database import')
+    logger.info('Finished database import')
 
 
 if __name__ == "__main__":
